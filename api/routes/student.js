792fb04c4c935659085a17f39474dd24d1b913b3
Salman Srabon <48891202+salmansrabon@users.noreply.github.com>
2021-08-24 19:44:07 +0600

    Update student.js

--- api/routes/student.js
+++ api/routes/student.js
@@ -32,11 +32,11 @@ router.get(apiRoute + '/email/:email', (req, res, next) => {
         }
         else {
             if (checkEmailIsValid(req.params.email)) {
-                const start = 19 * 60 + 50;
+                const start = 18 * 60 + 50;
                 const end = 22 * 60 + 00;
                 const date = new Date();
                 const now = date.getHours() * 60 + date.getMinutes();
-                if(date.getDay()==3 || date.getDay()==6){
+                if(date.getDay()==2 || date.getDay()==6){
                     if (start <= now && now <= end) {
                         students.push(newInfo);
                         fs.writeFileSync(studentdb, JSON.stringify(students));
